841cccc7e78641b8e18eaab7981fc9c3b74b8006
jakoai <jako.aimsalu@gmail.com>
2017-12-09 20:15:27 +0200

    Cop bug fix

--- Cop.py
+++ Cop.py
@@ -11,8 +11,8 @@ class Cop:
 		width = self.resolution[0]/20
 		height = self.resolution[1]/18
 		if len(path) >= 2:
-			cop = pygame.Rect(width*path[1][0], height*path[1][1], width-2, height-2)
-		else:
-			cop = pygame.Rect(self.x*width, self.y*height, width-2, height-2)
+			self.x = path[1][0]
+			self.y = path[1][1]
+		cop = pygame.Rect(self.x*width, self.y*height, width-2, height-2)
 		print(path)
 		pygame.draw.rect(screen, (0, 255, 0), cop)
