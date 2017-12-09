4fe5b4cb4713208f4547d8ec27eeb37fdf60543a
jakoai <jako.aimsalu@gmail.com>
2017-12-09 12:46:02 +0200

    Basic

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame, time, sys, math
+
+resolution = [1440, 900]
+pygame.init()
+screen = pygame.display.set_mode(resolution)
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+	screen.fill((0, 0, 0))
+
+	#Code here
+	
+	pygame.time.wait(60)
+	pygame.display.update()
