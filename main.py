0a3764b6795a2c4aa128ab5678e6346967a84738
MingiPedeKuskilEestis <mihkelorg.mo@gmail.com>
2017-12-09 16:50:42 +0200

    button

--- main.py
+++ main.py
@@ -9,20 +9,19 @@ screen = pygame.display.set_mode(resolution)
 
 player = Player.Player(10, 10, 1)
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_ESCAPE:
-				pygame.quit()
-				sys.exit()
-	screen.fill((255, 255, 255))
-	screen.blit(image, (264, 120))
-	player.draw(screen, player.move(0))
-	#Code here xd 1
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+    screen.fill((255, 255, 255))
+    screen.blit(image, (282, 120))
+    player.draw(screen, player.move(0))
+    pygame.draw.rect(screen, [0, 255, 0], (500, 600, 400, 150))
+    #Code here xd 1
 
-	pygame.time.wait(60)
-	pygame.display.update()
-
-
+    pygame.time.wait(60)
+    pygame.display.update()
