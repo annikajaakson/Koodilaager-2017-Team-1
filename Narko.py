17b3849d9510100f4dc2a93b0badbbe22a16bccd
Tulinpitsap2rast <heigotornik@gmail.com>
2017-12-09 16:10:23 +0200

    k

--- Narko.py
+++ Narko.py
@@ -1,1 +1,21 @@
-#Nimi
+#Heigo
+
+import pygame
+
+class Narko:
+    def __init__(self, PointReward, BarReward, NarkoType, Mode, xPos, yPos):
+        self.PointReward = PointReward                  #Punkte iga narko eest
+        self.BarReward = BarReward                      #Narkolaksu bar
+        self.NarkoType = NarkoType                      #Narko tüüp
+        self.Mode = Mode                                #Praegune narko objekti olukord
+        self.xPos = xPos                                #Koordinaat ruumis
+        self.yPos = yPos
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (255, 0, 0), (self.xPos, self.yPos, 100, 100))
+
+
+
+
+
+
