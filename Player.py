09f742c368fc34f0b6e445a54daae2d36e93ba3d
LadyCyanide <silvaliis.karu@gmail.com>
2017-12-09 15:27:54 +0200

    midagi

--- Player.py
+++ Player.py
@@ -1,2 +1,25 @@
 #Karu
 
+import pygame, time, math, main
+
+x = 10
+y = 10
+
+class Player:
+    def __init__ (self, x, y, kiirus):
+        self.x = x
+        self.y = y
+        self.kiirus = kiirus
+
+    def move(self, suund):
+        if suund == 0:
+            self.x += self.kiirus
+        elif suund == 2:
+            self.x -= self.kiirus
+        elif suund == 1:
+            self.y += self.kiirus
+        elif suund == 3:
+            self.y -= self.kiirus
+pygame.draw.rect (screen, [0, 0, 0],(x, y, 10, 10))
+
+
