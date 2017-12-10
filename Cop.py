f1908709ee8c7b20e8db75e86bebc96aa2f83551
Hendrik Eerikson <hendrik.eerikson@gmail.com>
2017-12-10 14:57:42 +0200

    Added a timer for the cop so that he can only move after a set amount of milliseconds

--- Cop.py
+++ Cop.py
@@ -1,18 +1,39 @@
 #Jako
 import pygame
+
 class Cop:
-	def __init__(self, x, y, resolution, mapn):
-		self.x = x
-		self.y = y
-		self.resolution = resolution
-		self.mapn = mapn
-	def pos(self):
-		return [self.x, self.y]
-	def draw(self, screen, path):
-		width = self.resolution[0]/len(self.mapn[0])
-		height = self.resolution[1]/len(self.mapn)
-		if len(path) > 2:
-			self.x = path[1][0]
-			self.y = path[1][1]
-		cop = pygame.Rect(self.x*width, self.y*height, width, height)
-		pygame.draw.rect(screen, (0, 255, 0), cop)
+    def __init__(self, x, y, resolution, mapn):
+        self.x = x
+        self.y = y
+        self.resolution = resolution
+        self.mapn = mapn
+        self.width = self.resolution[0] / len(self.mapn[0])
+        self.height = self.resolution[1] / len(self.mapn)
+        self.move_timer = 0  # the cop can move only once the timer has reached the cooldown
+        self.move_cooldown = 100  # time between steps in milliseconds
+
+        self.rect = pygame.Rect(self.x * self.width,
+                                self.y * self.height,
+                                self.width, self.height)
+
+    def pos(self):
+        return [self.x, self.y]
+
+    def update(self, delta, path):
+        self.move_timer += delta
+
+        if self.move_timer >= self.move_cooldown:
+            # if there is a next tile to move to, move to that tile
+            if len(path) > 2:
+                self.x = path[1][0]
+                self.y = path[1][1]
+
+            self.move_timer = 0  # reset the timer
+
+        # uodate the cop's rectangle to the current position
+        self.rect.x = self.x * self.width
+        self.rect.y = self.y * self.height
+
+    def draw(self, screen, ):
+        color = (0, 255, 0)
+        pygame.draw.rect(screen, color, self.rect)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Mingi Pede aka Mihkel
+# Mingi Pede aka Mihkel
 import pygame, time, sys, math
 import Player, Map, Cop
 
@@ -6,6 +6,9 @@ resolution = [1400, 900]
 pygame.init()
 image = pygame.image.load('Narkokorjaja.png')
 screen = pygame.display.set_mode(resolution)
+
+clock = pygame.time.Clock()
+delta = clock.tick()
 
 clicked = False
 narcotypes = ['2', '3', '4', '5', '6', '7', '8']
@@ -15,38 +18,42 @@ cop = Cop.Cop(1, 1, resolution, map1.mapn)
 player = Player.Player(1, 1, resolution, map1.mapn)
 
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
+    delta = clock.tick()  # time in milliseconds since the last frame
 
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_ESCAPE:
-				pygame.quit()
-				sys.exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
 
-	if clicked == False:
-		screen.fill((255, 255, 255))
-		screen.blit(image, (282, 120))
-		mouse = pygame.mouse.get_pos()
+    if clicked == False:
+        screen.fill((255, 255, 255))
+        screen.blit(image, (282, 120))
+        mouse = pygame.mouse.get_pos()
 
-		if 900 > mouse[0] > 500 and 750 > mouse[1] > 600:
-			pygame.draw.rect(screen, [0, 255, 0], (500, 600, 400, 150))
-		else:
-			pygame.draw.rect(screen, [0, 200, 0], (500, 600, 400, 150))
-		leftclick = pygame.mouse.get_pressed()
+        if 900 > mouse[0] > 500 and 750 > mouse[1] > 600:
+            pygame.draw.rect(screen, [0, 255, 0], (500, 600, 400, 150))
+        else:
+            pygame.draw.rect(screen, [0, 200, 0], (500, 600, 400, 150))
+        leftclick = pygame.mouse.get_pressed()
 
-		if leftclick == (1, 0, 0) and 900 > mouse[0] > 500 and 750 > mouse[1] > 600:
-			clicked = True
-	Keys = pygame.key.get_pressed()
+        if leftclick == (1, 0, 0) and 900 > mouse[0] > 500 and 750 > mouse[1] > 600:
+            clicked = True
+    Keys = pygame.key.get_pressed()
 
-	if clicked == True:
-		screen.fill((255, 255, 255))
-		walls = map1.draw(screen)
-		player_Pos = player.move(Keys[pygame.K_RIGHT]-Keys[pygame.K_LEFT], Keys[pygame.K_DOWN]-Keys[pygame.K_UP],map1)
-		player.draw(screen, player_Pos, Keys[pygame.K_RIGHT]-Keys[pygame.K_LEFT], Keys[pygame.K_DOWN]-Keys[pygame.K_UP])
-		cop.draw(screen, map1.path_find(cop.pos(), player_Pos))
+    if clicked == True:
+        screen.fill((255, 255, 255))
+        walls = map1.draw(screen)
+        player_Pos = player.move(Keys[pygame.K_RIGHT] - Keys[pygame.K_LEFT], Keys[pygame.K_DOWN] - Keys[pygame.K_UP],
+                                 map1)
+        player.draw(screen, player_Pos, Keys[pygame.K_RIGHT] - Keys[pygame.K_LEFT],
+                    Keys[pygame.K_DOWN] - Keys[pygame.K_UP])
+        cop.update(delta,  map1.path_find(cop.pos(), player_Pos))
+        cop.draw(screen)
 
-	pygame.time.wait(20)
-	pygame.display.update()
+    pygame.time.wait(20)
+    pygame.display.update()
